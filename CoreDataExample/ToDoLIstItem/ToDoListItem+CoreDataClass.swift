0c32d030da386f27edd0c3c6d588c4a6f704c5cf
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-08 14:20:03 -0400

    criação do crud com Core Data

---
+++ CoreDataExample/ToDoLIstItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by Gustavo Anjos on 08/01/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -8,10 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // Pegar todos os dados do BD
+    func getAllItems(){
+        do{
+            let items = try context.fetch(ToDoListItem.fetchRequest())
+        }catch{
+            // error
+        }
+    }
+    
+    // Criar um novo item
+    func createItem(name: String){
+        
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do{
+            try context.save()
+        }catch{
+            
+        }
+        
+    }
+    
+    func deleteItem(item: ToDoListItem){
+        
+        context.delete(item)
+        
+        do{
+            try context.save()
+        }catch{
+            
+        }
+        
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String){
+        item.name = newName
+        
+        do{
+            try context.save()
+        }catch{
+            
+        }
     }
 
 
